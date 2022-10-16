a5a3f0e2a62099392129843df870159583d1e56e
Taylor Mastin <thmastin@gmail.com>
2022-10-15 23:19:47 -0400

    Add function and variables for a computer random pick

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,18 @@
+//Create function named getComputerChoice that returns random string
+
+getComputerChoice() (
+
 //Create variable that is a string compPick with inital value of null
+let compPick = "";
+//Create a variable named rock type string to hold "Rock"
+let rock = "Rock";
+//Create a variable named paper type string to hold "Paper"
+let paper = "Paper";
+//Create a variable named scissors type string to hold "Scissors"
+let scissors = "Scissors";
 //set compPick to random of either "Rock", "Paper" or Scissors
+
+
 //Output compPick as a string 
+
+)
