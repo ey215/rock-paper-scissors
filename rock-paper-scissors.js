f88c81aa236145861d342f5478b8b23c39d6fac8
Taylor Mastin <thmastin@gmail.com>
2022-10-15 23:29:28 -0400

    Add variable for random number

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,13 +4,15 @@ getComputerChoice() (
 
 //Create variable that is a string compPick with inital value of null
 let compPick = "";
-//Create a variable named rock type string to hold "Rock"
-let rock = "Rock";
-//Create a variable named paper type string to hold "Paper"
-let paper = "Paper";
-//Create a variable named scissors type string to hold "Scissors"
-let scissors = "Scissors";
-//set compPick to random of either "Rock", "Paper" or Scissors
+
+//Create a variable named pick type integer with intial value of 0
+let pick = 0;
+
+//Set pick to a random number between 0 and 2
+
+
+//use if/esle to set compPick to "Rock", "Paper", or "Scissors" based value in pick
+
 
 
 //Output compPick as a string 
