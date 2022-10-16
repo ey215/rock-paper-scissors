e7518265c36b29bcd1d251d9799b825070d2f5c1
Taylor Mastin <thmastin@gmail.com>
2022-10-16 00:07:49 -0400

    Restart fresh

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,20 +1,6 @@
-//Create function named getComputerChoice that returns random string
+//Create variable type string named compPick with the intial value ""
 
-getComputerChoice() (
+compPick = "";
 
-//Create variable that is a string compPick with inital value of null
-let compPick = "";
-
-//Create a variable named pick type integer with intial value of 0
-let pick = 0;
-
-//Set pick to a random number between 0 and 2
-
-
-//use if/esle to set compPick to "Rock", "Paper", or "Scissors" based value in pick
-
-
-
-//Output compPick as a string 
-
-)
+//Create function getComputerChoice that returns either "Rock", "Paper", or "Scissors"
+//set compPick to result of getComputerChoice
