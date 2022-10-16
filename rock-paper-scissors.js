ae7da4fa0a1a88cf52048f1f44f627d48a3adac9
Taylor Mastin <thmastin@gmail.com>
2022-10-16 00:21:25 -0400

    Add code to generate random integer between 0 and 3

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,4 +3,25 @@
 compPick = "";
 
 //Create function getComputerChoice that returns either "Rock", "Paper", or "Scissors"
+function getComputerChoice() {
+    //Create variable num wity type integer and intial value of 0
+    let num = 0;
+    //Create variable pick type string and intitial value of ""
+    let pick = "";
+
+    //Create function getRandomInt to return integer
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    //Set num to result of getRandomInt with argument of 3
+
+    num = getRandomInt(3);
+    return num;
+
+    //Use if/else to set pick to either "Rock", "Paper", or "Scissors" based on int in num 
+}
+
+
 //set compPick to result of getComputerChoice
+console.log(getComputerChoice());
